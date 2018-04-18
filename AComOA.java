fafbb1631677ceda3f1919bc70e92068491e6f5a
Qingqian <zhaoqingqian21@hotmail.com>
2018-04-18 00:38:38 -0500

    solutions to more questions

--- AComOA.java
+++ AComOA.java
@@ -181,6 +181,11 @@ public List<Integer> getFullLables(List<String> targetList, List<String> availab
 			map.put(current, map.get(current) + 1);
 		}
 		while(map.size() == dict.size()) {
+			if(minLength > end - start) {
+				minLength = Math.min(minLength, end - start);
+				result.add(0, start);
+				result.add(1, end);
+			}
 			String previous = availableTagList.get(start++);
 			if(map.containsKey(previous)) {
 				map.put(previous, map.get(previous) - 1);
@@ -188,11 +193,6 @@ public List<Integer> getFullLables(List<String> targetList, List<String> availab
 					map.remove(previous);
 				}
 			}
-		}
-		if(minLength > end - start) {
-			minLength = Math.min(minLength, end - start);
-			result.add(0, start);
-			result.add(1, end);
 		}
 	}
 
@@ -270,4 +270,240 @@ public List<String> wordGame(String str, int k) {
 	}
 	return result;
 
+}
+// union find - item association
+private class UnionFind{
+	private Map<String, String> parent;
+	private int[] count;
+	private String rootWithMaxCount;
+	private int maxCount;
+
+	public UnionFind(String[][] items) {
+		this.parent = new HashMap<String, String>();
+		for(String[] item : items) {
+			String first = item[0];
+			String second = item[1];
+			map.put(first, first);
+			map.put(second, second);
+		}
+		this.count = new int[map.size()];
+		Arrays.fill(count, 1);
+		rootWithMaxCount = null;
+		maxCount = 1;
+	}
+
+	public String find(String str) {
+		if(map.get(str).equals(str)) {
+			return str;
+		}
+		String root = find(map.get(str));
+		map.put(str, root);
+		return map.get(str);
+	}
+
+	public void union(String str1, String str2) {
+		String root1 = find(str1);
+		String root2 = find(str2);
+		if(root1.equals(root2)) {
+			return;
+		}
+		map.put(root2, root1);
+		count[root1] += count[root2];
+		if(maxCount < count[root1]) {
+			maxCount = count[root1];
+			rootWithMaxCount = root1;
+		}
+	}
+
+	public List<String> getWordsWithLargestUnion() {
+		List<String> result = new ArrayList<String>();
+		for(String key : parent.keyset()) {
+			String root = find(key);
+			if(root.equals(rootWithMaxCount)) {
+				result.add(key);
+			}
+		}
+		return result;
+	}
+}
+public List<String> itemAssociation(String[][] items) {
+	UnionFind uf = new UnionFind(items);
+	for(String[] item : items) {
+		uf.union(item[0], item[1]);
+	}
+	return uf.getWordsWithLargestUnion();
+}
+
+public int throwBaseBall(String[] score) {
+	if(score == null || score.length == 0) {
+		return 0;
+	}
+	Stack<Integer> stack = new Stack<Integer>();
+	int result = 0;
+
+	for(int i = 0; i < score.length; i++) {
+		String current = score[i];
+		int value = 0;
+		if(!stack.isEmpty() && current.equals("X")) {
+			value = stack.peek() * 2;
+			result += value;
+			stack.push(value);
+		} else if(!stack.isEmpty() && current.equals("Z")) {
+			value = stack.pop();
+			result -= value;
+		} else if(current.equals("+")) {
+			if(stack.isEmpty()) {
+				value = 0;
+			} else {
+				int first = stack.pop();
+				if(stack.isEmpty()) {
+					value = first;
+				} else {
+					value = first + stack.peek();
+				}
+				stack.push(first);
+			}
+			result += value;
+			stack.push(value);
+		} else {
+			value = Integer.parseInt(current);
+			result += value;
+			stack.push(value);
+		}
+	}
+	return result;
+}
+
+private class TreeNode {
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+	public TreeNode(int value) {
+		this.val = value;
+		this.left = this.right = null;
+	}
+}
+
+public int distance(int[] input, int number1, int number2) {
+	if(input == null || inpput.length < 2) {
+		return -1;
+	}
+	Arrays.sort(input);
+	TreeNode root = buildTree(input, 0, input.length - 1);
+	return findDistance(root, number1, number2);
+}
+
+private TreeNode buildTree(int[] input, int start, int end) {
+	if(start > end) {
+		return null;
+	}
+	int middle = start + (end - start) / 2;
+	TreeNode current = new TreeNode(input[middle]);
+	current.left = buildTree(input, start, middle - 1);
+	current.right = buildTree(intput, middle + 1, end);
+	return current;
+}
+
+private int findDistance(TreeNode root, int number1, int number2) {
+
+	int firstLevel = getDepthFromRoot(root, number1, 0);
+	int secondLevel = getDepthFromRoot(root, number2, 0);
+	// one of the number doesn't exist in the bst
+	if(firstLevel == 0 || secondLevel == 0) {
+		return -1;
+	}
+	TreeNode lcaNode = getLCABST(root, number1, number2);
+	if(lcaNode == null) {
+		return -1;
+	}
+	int lcaLevel = getDepthFromRoot(root, lcaNode.val, 0);
+	return firstLevel + secondLevel - 2 * lcaLevel;
+}
+
+private int getDepthFromRoot(TreeNode root, int number, int level) {
+	if(root == null) {
+		return 0;
+	}
+	if(root.val == number) {
+		return level;
+	}
+	if(root.val > number) {
+		return getDepthFromRoot(root.left, number, level + 1);
+	} else {
+		return getDepthFromRoot(root.right, number, level + 1);
+	}
+}
+
+private TreeNode getLCABST(TreeNode root, int number1, int number2) {
+	if(root == null) {
+		return null;
+	}
+	if(root.val > number1 && root.val > number2) {
+		return getLCABST(root.left, number1, number2);
+	}
+	if(root.val < number1 && root.val < number2) {
+		return getLCABST(root.right, number1, number2);
+	}
+	return root;
+}
+
+public List<String> mostCommonWord(String paragraph, List<String> banned) {
+	List<String> result = new ArrayList<String>();
+ 	if(paragraph == null || paragraph.length() == 0) {
+ 		return result;
+ 	}
+ 	String[] words = paragraph.trim().toLowerCase().replaceAll("\\p{P}", " ").split("\\s+");
+ 	Map<String, Integer> map = new HashMap<String, Integer>();
+ 	int maxCount = 0;
+ 	for(String word : words) {
+ 		if(!map.containsKey(word)) {
+ 			map.put(word, 0);
+ 		}
+ 		map.put(word, map.get(word) + 1);
+ 		if(maxCount < map.get(word)) {
+ 			maxCount = map.get(word);
+ 		}
+ 	}
+ 	for(String word : map.keySet()) {
+ 		if(map.get(word) == count) {
+ 			result.add(word);
+ 		}
+ 	}
+ 	return result;
+}
+
+public List<String> reorderLogFile(int logFileSize, List<String> lines) {
+	if(logFileSize == 0 || lines == null || lines.size() == 0) {
+		return lines;
+	}
+	List<String> result = new ArrayList<String>();
+	List<String> numberLines = new ArrayList<String>();
+	PriorityQueue<String[]> wordPriorityQueue = new PriorityQueue<String[]>(logFileSize, new Comparator<String[]>(){
+		public int compare(String[] line1, String[] line2) {
+			int compareResult = line1[1].compareTo(line2[1]);
+			if(compareResult != 0) {
+				return compareResult;
+			} else {
+				return line1[0].compareTo(line2[0]);
+			}
+		}
+	});
+	for(String line : lines) {
+		int firstSpaceIndex = line.indexOf(" ");
+		String lineId = line.substring(0, firstSpaceIndex);
+		String linewords = line.substring(firstSpaceIndex + 1);
+		if(linewords.length() > 0 && Character.isDigit(linewords.charAt(0))) {
+			numberLines.add(line);
+		} else if(linewords.length() > 0 && Character.isLetter(linewords.charAt(0))) {
+			wordPriorityQueue.offer(new String[] {lineId, linewords});
+		}
+	}
+	while(!wordPriorityQueue.isEmpty()) {
+		String[] word = wordPriorityQueue.poll();
+		result.add(word[0] + " " + word[1]);
+	}
+	for(int i = 0; i < numberLines.size(); i++) {
+		result.add(numberLines.get(i));
+	}
+	return result;
 }
